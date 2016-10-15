c47bdbb22ca4f380ff7bf98eb583507567b8f0ad
dirago <diragor@gmail.com>
2016-10-15 15:14:09 +0200

    header started

--- index.php
+++ index.php
@@ -4,19 +4,33 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
-    	<meta property="og:title" content="" />
-    	<meta property="og:type" content="" />
-    	<meta property="og:url" content="" />
+    	<meta property="og:title" content="Child.y | Assistant parental" />
+    	<meta property="og:type" content="website" />
+    	<meta property="og:url" content="http://www.raphaeldirago.com/Childy" />
     	<meta property="og:image" content="" />
-    	<meta property="og:description" content="">
-        <title></title>
+    	<meta property="og:description" content="Gérez au jour le jour le comportement de votre
+        enfant grâce à une plateforme simple et ludique mais avant tout pédagogique">
+        <title>Child.y | Assistant parental</title>
         <link rel="stylesheet" href="css/style.min.css">
         <script src="js/jquery-3.1.0.min.js"></script>
     </head>
     <body>
-
+        <header>
+            <object data="img/logo_opt.svg" height="80" type="image/svg+xml" class="logo">
+            <!-- S'affiche seulement si le browser supporte SVG -->
+            <!-- Sinon, le PNG le remplacera -->
+            <img src="img/logo.png" alt="Logo Childy" />
+            </object>
+            <div class="log-panel">
+                <a href="#" class="log-panel--item">Connexion</a>
+                <a href="#" class="log-panel--item">Inscription</a>
+            </div>
+        </header>
 
 
         <script type="text/javascript" src="js/script.js"></script>
+        <script type="text/javascript" src="http://localhost:35729/livereload.js">
+
+        </script>
     </body>
 </html>
