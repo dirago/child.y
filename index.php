1b64379442e7cc93e642554dd9edba9958c844fb
dirago <diragor@gmail.com>
2016-10-16 03:55:36 +0200

    understood how babel works, finally....

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
         <?php include 'content.php'; ?>
         <?php include 'footer.php'; ?>
         <script src="js/jquery-3.1.0.min.js"></script>
-        <script type="text/javascript" src="js/script.js"></script>
+        <script type="text/javascript" src="js/script-compiled.js"></script>
         <script type="text/javascript" src="http://localhost:35729/livereload.js"></script>
     </body>
 </html>
---
+++ js/custom/demo.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// I want to isolate each line of smileys so will be independent
+var smileyRows = Array.from(document.getElementsByClassName('responsability-smile')),
+    j = 0;
+// i create ids for each generated line of smiles
+smileyRows.map(i => {if (i.id != null){j++; return i.id = "a" + j;} else {j = 1; i.id = "a" + j}});
+smileyRows.map(i => {
+    let svgs = Array.from(document.querySelectorAll('#' + i.id + ' > svg'));
+    svgs.map(function(i){
+        i.addEventListener('click', function(e){
+            svgs.map(function(i){
+                i.classList.remove(i.getAttribute('data-state'));
+            });
+            e.currentTarget.classList.add(e.currentTarget.getAttribute('data-state'));
+            console.log(e.currentTarget.getAttribute('data-state'));
+        });
+    });
+});
