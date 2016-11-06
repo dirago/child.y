d50bd8be6edba8a4ec6a5e1d1261ace61f20d84f
Raphaël DI RAGO <diragor@gmail.com>
2016-11-06 22:59:41 +0100

    avancement demo

--- js/custom/app.js
+++ js/custom/app.js
@@ -1,7 +1,8 @@
-window.onload = function(){
+
     // I want to isolate each line of smileys so will be independent
     var rows = Array.from(document.getElementsByClassName('responsability-smile')),
-        rowId = 0;
+        rowId = 0,
+        recordedData = new Array();
     // I create IDs for each generated line of smiles
     rows.map(el => {
         if (el.id != null){
@@ -22,29 +23,40 @@ window.onload = function(){
                 });
                 let item = evt.currentTarget.getAttribute('data-item');
                 let note = evt.currentTarget.getAttribute('data-note');
-                console.log('item selectionné :', item);
-                console.log('notation :', note);
+                // console.log('item selectionné :', item);
+                // console.log('notation :', note);
                 evt.currentTarget.classList.add(evt.currentTarget.getAttribute('data-color'));
                 evt.currentTarget.setAttribute('data-selected', true);
-                afficheNotes();
+                setNotes();
             });
         });
     });
-    function afficheNotes(){
-            let svgs = rows.map(row => {
-                return Array.from(document.querySelectorAll('#' + row.id + ' > svg'));
+    function onlySelected(e){
+        return e.getAttribute('data-selected') === "true";
+    };
+    function setNotes(){
+        let svgs = rows.map(row => {
+            return Array.from(document.querySelectorAll('#' + row.id + ' > svg'));
+        });
+        svgs.map(svg => {
+            let filter = svg.filter(onlySelected);
+            filter.map(el => {
+                let check = recordedData.filter(function(data){
+                    return data.item === el.getAttribute('data-item');
+                });
+                if (check.length === 0){
+                    recordedData.push({
+                        "item": el.getAttribute('data-item'),
+                        "note": el.getAttribute('data-note')
+                    });
+                } else {
+                    for (let i = 0; i < recordedData.length; i++){
+                        if (recordedData[i].item === el.getAttribute('data-item')){
+                            recordedData[i].note = el.getAttribute('data-note');
+                        };
+                    };
+                };
             });
-            svgs.map(svg => {
-                let filter = svg.filter(onlySelected);
-                console.log(filter);
-                filter.map(el =>{
-                    console.log("L'item " + el.getAttribute('data-item'), "va avec la note " + el.getAttribute('data-note'));
-                    
-                })
-            })
+        });
+        return recordedData;
     };
-
-    function onlySelected(elt){
-        return elt.getAttribute('data-selected') === "true";
-    };
-};
--- js/script-compiled.js
+++ js/script-compiled.js
@@ -1,53 +1,65 @@
 "use strict";
 
-window.onload = function () {
-    // I want to isolate each line of smileys so will be independent
-    var rows = Array.from(document.getElementsByClassName('responsability-smile')),
-        rowId = 0;
-    // I create IDs for each generated line of smiles
-    rows.map(function (el) {
-        if (el.id != null) {
-            rowId++;
-            el.id = "r" + rowId;
-        } else {
-            rowId = 1;
-            el.id = "r" + rowId;
-        };
+// I want to isolate each line of smileys so will be independent
+var rows = Array.from(document.getElementsByClassName('responsability-smile')),
+    rowId = 0,
+    recordedData = new Array();
+// I create IDs for each generated line of smiles
+rows.map(function (el) {
+    if (el.id != null) {
+        rowId++;
+        el.id = "r" + rowId;
+    } else {
+        rowId = 1;
+        el.id = "r" + rowId;
+    };
+});
+rows.map(function (row) {
+    var svgs = Array.from(document.querySelectorAll('#' + row.id + ' > svg'));
+    svgs.map(function (svg) {
+        svg.addEventListener('click', function (evt) {
+            svgs.map(function (svg) {
+                svg.classList.remove(svg.getAttribute('data-color'));
+                svg.setAttribute('data-selected', '');
+            });
+            var item = evt.currentTarget.getAttribute('data-item');
+            var note = evt.currentTarget.getAttribute('data-note');
+            // console.log('item selectionné :', item);
+            // console.log('notation :', note);
+            evt.currentTarget.classList.add(evt.currentTarget.getAttribute('data-color'));
+            evt.currentTarget.setAttribute('data-selected', true);
+            setNotes();
+        });
     });
-    rows.map(function (row) {
-        var svgs = Array.from(document.querySelectorAll('#' + row.id + ' > svg'));
-        svgs.map(function (svg) {
-            svg.addEventListener('click', function (evt) {
-                svgs.map(function (svg) {
-                    svg.classList.remove(svg.getAttribute('data-color'));
-                    svg.setAttribute('data-selected', '');
+});
+function onlySelected(e) {
+    return e.getAttribute('data-selected') === "true";
+};
+function setNotes() {
+    var svgs = rows.map(function (row) {
+        return Array.from(document.querySelectorAll('#' + row.id + ' > svg'));
+    });
+    svgs.map(function (svg) {
+        var filter = svg.filter(onlySelected);
+        filter.map(function (el) {
+            var check = recordedData.filter(function (data) {
+                return data.item === el.getAttribute('data-item');
+            });
+            if (check.length === 0) {
+                recordedData.push({
+                    "item": el.getAttribute('data-item'),
+                    "note": el.getAttribute('data-note')
                 });
-                var item = evt.currentTarget.getAttribute('data-item');
-                var note = evt.currentTarget.getAttribute('data-note');
-                console.log('item selectionné :', item);
-                console.log('notation :', note);
-                evt.currentTarget.classList.add(evt.currentTarget.getAttribute('data-color'));
-                evt.currentTarget.setAttribute('data-selected', true);
-                afficheNotes();
-            });
+            } else {
+                for (var i = 0; i < recordedData.length; i++) {
+                    if (recordedData[i].item === el.getAttribute('data-item')) {
+                        recordedData[i].note = el.getAttribute('data-note');
+                    };
+                };
+            };
         });
     });
-    function afficheNotes() {
-        var svgs = rows.map(function (row) {
-            return Array.from(document.querySelectorAll('#' + row.id + ' > svg'));
-        });
-        svgs.map(function (svg) {
-            var filter = svg.filter(onlySelected);
-            console.log(filter);
-            filter.map(function (el) {
-                console.log("L'item " + el.getAttribute('data-item'), "va avec la note " + el.getAttribute('data-note'));
-            });
-        });
-    };
-
-    function onlySelected(elt) {
-        return elt.getAttribute('data-selected') === "true";
-    };
+    return recordedData;
 };
 'use strict';
 
